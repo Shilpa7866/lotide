7ade98d7f15fb64368e4c7c99b04877281cc76ae
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-18 17:30:17 -0400

    changes made in middle.js file

---
+++ head.js
@@ -0,0 +1,18 @@
+/*const assertEqual = function()
+function head (array) {
+  let arrayOne = array[0];
+  return arrayOne
+};
+
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+*/
+
+
+
+x = 12;
+if(true) {
+  x + 2;
+  x = 4;
+}
+console.log(x);
