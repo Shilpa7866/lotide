af6a8cecac1af4dacd14e368d8f5dce46acd7da1
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-19 18:21:06 -0400

    adding README.md file

--- assertEqual.js
+++ assertEqual.js
@@ -1,18 +1,3 @@
-/*const sum = function(a, b) {
-  return a + b;
-}
-console.assert(sum(1, 2) === 3);
-console.assert(sum(1, 20) === 3);
-*/
-
-
-/*const sumBuggy = function(a,b) {
-        return a * b;
- }
- console.assert(sumBuggy(1,2)===3);
-*/
-
-
 const assertEqual = function(actual, expected) {
     if (actual === expected) {
         return "Assertion Passed: [actual] === [expected]";
@@ -23,7 +8,7 @@ const assertEqual = function(actual, expected) {
 };
 
 
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+/*console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
 console.log(assertEqual(1, 2));
 console.log(assertEqual("apple", "banana"));
 console.log(assertEqual(10, 10));
@@ -31,6 +16,6 @@ console.log(assertEqual("fruit", "fruit"));
 // test changes
 console.log(assertEqual("fruit", "vegetables"));
 console.log(assertEqual("veggies", 22));
-
+*/
 
 module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,55 +1,5 @@
-/*const assertEqual = function()
-//function head (array) {
-  let arrayOne = array[0];
-  return arrayOne
-};
 
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-
-
-
-
-x = 12;
-if(true) {
-  x + 2;
-  x = 4;
-}
-console.log(x);
-
-//const assertEqual = function(actual, expected) {
-//const assertEqual = require('./assertEqual'); 
-//if (actual === expected) {
-        //return "Assertion Passed: [actual] === [expected]";
-    //}
-    //else if (actual !== expected) {
-       // return "Assertion Failed: [actual] !== [expected]";
-    //};
-
-const mean = function(arr) {
-  let avgList = arr [0]
-  for (let i = 0; i < arr.length; i++){
-     avgList += arr[i]
-  }
-    return avgList / arr.length
-}
-
-const round = function(number) {
-return Math.round(number * 100) / 100;
-};
-console.log(mean([6,2,3,4,9,6,1,0,5]));
-//console.log(assertEqual(mean([6,2,3,4,9,6,1,0,5])));
-*/
-
-
-const assertEqual = function(actual, expected) {
-
-  if( actual === expected)
-    return "Assertion Passed: ["+actual+"] === ["+expected+"]"
-  else
-    return "Assertion failed: ["+actual+"] !== ["+expected+"]"
-
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arr1) {
   return arr1[0];
@@ -59,6 +9,8 @@ const head = function(arr1) {
 console.log(assertEqual(head([5,6,7]), 5));
 
 console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+
+
 module.exports = head;
 
 
