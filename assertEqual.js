564ae50a202f20edde7f386bdbc690b6120d2182
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-19 18:44:39 -0400

    updating tail.js file

--- assertEqual.js
+++ assertEqual.js
@@ -8,14 +8,4 @@ const assertEqual = function(actual, expected) {
 };
 
 
-/*console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual(1, 2));
-console.log(assertEqual("apple", "banana"));
-console.log(assertEqual(10, 10));
-console.log(assertEqual("fruit", "fruit"));
-// test changes
-console.log(assertEqual("fruit", "vegetables"));
-console.log(assertEqual("veggies", 22));
-*/
-
 module.exports = assertEqual;
--- tail.js
+++ tail.js
@@ -1,19 +1,22 @@
-//const assertEqual = function()
-//const tail = function(array);
-//const result = tail(["Hello", "Lighthouse", "Labs"]);
-//assertEqual(result, ["Lighthouse", "Labs"]);
 
-const assertEqual = require('./assertEqual');
 
-const tail = function(array) {
-  let arrayReturn = [];
-  if (array.length <= 1) {
-    return arrayReturn;
-  } else {
-    arrayReturn = array.slice(1);
-  } return arrayReturn;
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+      return "Assertion Passed: [actual] === [expected]";
+  }
+  else if (actual !== expected) {
+      return "Assertion Failed: [actual] !== [expected]";
+  }
 };
+
+const tail = function(arr1) {
+  let arr2 = [];
+  for(let i=1; i<arr1.length; i++)
+    arr2.push(arr1[i]);
+  return arr2;
+}
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(result, ["Lighthouse", "Labs"]));
+console.log(result, ["Lighthouse", "Labs"]);
+
 
 module.exports = tail;
