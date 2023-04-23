4943683678fc5ba74ac84d481b6476e3b9ad217f
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-22 22:40:05 -0400

    moved irrelevant file

--- timer/timer1.js
+++
@@ -1,9 +0,0 @@
-/*const inputArr = process.argv.slice(2).filter(t => !isNaN(t)).filter(t => t >=0 );
-
-for (const time of inputArr){
-  setTimeout(() => { process.stdout.write('\x07'); }, time * 1000);
-};
-*/
-
-
-process.stdout.write('\x07');
