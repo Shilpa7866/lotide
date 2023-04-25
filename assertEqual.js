805e394da2e7f02d5a84fd5df9ccf66a9339f4f3
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-25 11:23:58 -0400

    adding updated assertEqual.js file

--- assertEqual.js
+++ assertEqual.js
@@ -1,16 +1,12 @@
 
 const assertEqual = function(actual, expected) {
     if (actual === expected) {
-        return "Assertion Passed: [actual] === [expected]";
+        console.log("Assertion Passed: [+actual+] === [+expected]+");
     }
     else if (actual !== expected) {
-        return "Assertion Failed: [actual] !== [expected]";
+        console.log("Assertion Failed: [+actual+] !== [+expected+]");
     }
 };
-console.log(assertEqual("lighthouse labs", "Bootcamp"));
-
-console.log(assertEqual(1, 1));
-console.log(assertEqual(1, -1));
 
 
 module.exports = assertEqual;
