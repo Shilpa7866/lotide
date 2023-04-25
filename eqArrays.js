2bf1efc3966443c59f4692fef50aff08128b4a72
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-25 11:26:25 -0400

    adding updated eqArrays.js file

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,5 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
 
-  return;
-};
-
+const assertEqual = require('./assertEqual');
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -19,18 +11,6 @@ const eqArrays = function (arr1, arr2) {
   }
   return true;
 };
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [4, 5, 6]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); 
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); 
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-console.log(eqArrays([1, 2, 3], [4, 5, 6]));
-
-
 
 
 module.exports = eqArrays;
