49662c6f2a49d43ed6115b6f03c8ffad6d6a9fb4
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-25 15:18:36 -0400

    updating middleTest.js file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,20 @@
-
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-console.log(middle([1,3,5])); // expected => []
-console.log(middle([1,2,3])); // expected => [2]
-console.log(middle([1,2,3,4,5,6])); // expected => [3]
 
-module.exports = middle;
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+describe("#middle", () => {
+  it("returns 3 for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  describe("#middle", () => {
+   it("return [] for [1,2]", () => {
+    assert.deepEqual(middle([1,2]), []);
+   });
+});
+});
+});
+
