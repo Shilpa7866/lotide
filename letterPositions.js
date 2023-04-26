fb97755a5f57e29369f8dd2e234e4e8fa5908960
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-26 14:48:23 -0400

    adding modified letterPositions.js file

--- letterPositions.js
+++ letterPositions.js
@@ -1,18 +1,16 @@
-const letterPositions = function(sentence) {
+const letterPositions = function (sentence) {
   const results = {};
-    for (i = 0; i < sentence.length; i++) {
-	    if (results[sentence[i]]) {
+  for (i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
       results[sentence[i]].push(i);
     } else {
       results[sentence[i]] = [i];
     }
-      
+
   }
-     return results;
+  return results;
 };
 
-console.log(letterPositions("hello"));
-console.log(letterPositions("lighthouse in the house"));
 
 module.exports = letterPositions;
 
