3978eaa38eac519b0b9c387553a3c6706e46bfef
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-26 14:11:53 -0400

    adding modified assertObjectsEqualTest.js file

--- test/assertObjectsEqualTest.js
+++ test/assertObjectsEqualTest.js
@@ -1,8 +1,9 @@
-
+// import assertObjectsEqual function
 const assertObjectsEqual = require('../assertObjectsEqual');
 
-assertObjectsEqual({ a: '1', b: 2 },{ b: 2, a: '1' });
-assertObjectsEqual({ a: '1', b: 2 },{ b: 2, a: '1', c: 3 });
-assertObjectsEqual("lighthouse", "labs");
-assertObjectsEqual("lighthouse", "lighthouse");
+assertObjectsEqual({ a: '1', b: 2 },{ b: 2, a: '1' });  // passed
+assertObjectsEqual({ a: '1', b: 2 },{ b: 2, a: '1', c: 3 });  //failed
+assertObjectsEqual("lighthouse", "labs");  //failed
+assertObjectsEqual("lighthouse", "lighthouse");  //passed
 
+module.exports = assertObjectsEqual;
