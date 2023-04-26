6112ab55d6154afb6c3da6aec93c416eeeaea26a
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-26 14:49:34 -0400

    adding modifies tail.js file

--- tail.js
+++ tail.js
@@ -1,17 +1,6 @@
-
-const assertEqual = require('./assertEqual');
-
-const tail = function(arr1) {
-  let arr2 = [];
-  for(let i = 1; i < arr1.length; i++)
-    arr2.push(arr1[i]);
-  return arr2;
-}
-
-//console.log(assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello"));
-//console.log(assertEqual(tail([5,6,7]), 5));
-
-//console.log((tail(["Hello", "Lighthouse", "Labs"]), "Hello"));
-//console.log((tail([5,6,7]), 5));
+const tail = function (actual) {
+  return actual.slice(1);
+};
 
 module.exports = tail;
+
