14ae652ff4774c080d2adac68bf94267f90a7e19
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-26 14:09:00 -0400

    adding findKeyTest.js file

---
+++ test/findKeyTest.js
@@ -0,0 +1,20 @@
+// import assert library from chai package.
+const assert = require('chai').assert;
+
+// import findKey function.
+const findKey = require('../findKey');
+
+describe("findKey function", () => {
+  it("returns the assigned key to the object value", () => {
+    const answer = findKey({
+      "Blue Hill": { stars: 1 },
+      "Akaleri": { stars: 3 },
+      'noma': { stars: 2 },
+      "elbBulli": { stars: 3 },
+      "Ora": { stars: 2 },
+      "Akelarre": { stars: 3 }
+    }, x => x.stars === 2);
+    assert.deepEqual(answer, 'noma');
+  });
+});
+  
