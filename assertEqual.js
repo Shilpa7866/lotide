eb1dbcc8839c940a28e05a7baa808db1b6f148ea
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-26 14:43:43 -0400

    adding updated assertEqual.js file

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,5 @@
 
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
     if (actual === expected) {
         console.log("Assertion Passed: [+actual+] === [+expected]+");
     }
