dd030dfcd5cd43df79ab05de863b546bb929cee3
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-26 14:49:59 -0400

    adding modified without.js file

--- without.js
+++ without.js
@@ -1,25 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-  return;
-};
-
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
 const without = function (source, itemsToRemove) {
   let newArray = [];
   for (let i = 0; i < source.length; i++) {
@@ -27,10 +5,8 @@ const without = function (source, itemsToRemove) {
       newArray.push(source[i]);
     }
   }
- return newArray;
+  return newArray;
 }
 
-console.log(without([1, 2, 3], [1]));
-console.log(without(['1', '2', '3'], [1, 2, '3']));
 
 module.exports = without;
