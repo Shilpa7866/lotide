d0dec8bfa95f0268172722a4b08436230a7139e9
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-26 14:47:18 -0400

    adding updated flatten.js file

--- flatten.js
+++ flatten.js
@@ -1,17 +1,16 @@
 const flatten = (arr) => {
 	let flatArr = [];
-	for (var i = 0; i < arr.length; i++){
-		if (Array.isArray(arr[i])){
-			for (var x = 0; x < arr[i].length; x++){
+	for (var i = 0; i < arr.length; i++) {
+		if (Array.isArray(arr[i])) {
+			for (var x = 0; x < arr[i].length; x++) {
 				flatArr.push(arr[i][x]);
 			};
 		} else {
 			flatArr.push(arr[i]);
 		}
 	};
-	return flatArr; 
+	return flatArr;
 }
 
-console.log(flatten([1,2,3, [4,5], 6, [7,8], 9]))
 
 module.exports = flatten;
