63156c0a70b3d8a40379eaa928d356cdc43dcef4
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-26 14:10:23 -0400

    adding modified eqArraysTest.js file

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,17 +1,23 @@
-const assertEqual = require('../assertEqual');
+//import eqArrays function.
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [4, 5, 6]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); 
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); 
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-console.log(eqArrays([1, 2, 3], [4, 5, 6]));
+//import assert library from chai package.
+const assert = require('chai').assert;  
 
 
-
+describe("eqArrays function", () => {
+  it("returns true when its identical arrays", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("returns true when its empty arrays", () => {
+    assert.deepEqual(eqArrays([], []), true);
+  });
+  it("returns false when the arrays with different values", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+  it("returns false when comparing arrays with different types", () => {
+    assert.deepEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
+  });
+});
 
 module.exports = eqArrays;
