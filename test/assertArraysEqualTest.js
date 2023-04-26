e7a22ded0f89afb201318cb72361e047eb64e2b5
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-26 14:13:10 -0400

    adding modified assertArraysEqualTest.js file

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,7 +1,12 @@
+// import assertArraysEqual function.
 const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual([1, 2, 3], [1,2,3]);
+assertArraysEqual([1, 2, 3], [1,2,3]); //passed
 
-assertArraysEqual(["1", "2", "3"], [1, 2, "3"]);
+assertArraysEqual(["1", "2", "3"], [1, 2, "3"]); //failed
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);   // passed
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);     // failed
 
 module.exports = assertArraysEqual;
