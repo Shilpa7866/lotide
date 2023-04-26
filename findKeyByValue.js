a76bdd58d8fffb61a71564714c999b0fab1d2330
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-26 14:46:38 -0400

    adding updated findKeyByValue.js file

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,35 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-      return "Assertion Passed: [actual] === [expected]";
+const findKeyByValue = function (object, value) {
+  for (let key in object) {
+    if (object[key] === value) {
+      return key;
+    }
   }
-  else if (actual !== expected) {
-      return "Assertion Failed: [actual] !== [expected]";
-  }
+  return undefined;
 };
-
-const findKeyByValue = function(object, value){
-let loop =Object.values(object)
-let key = Object.keys(object)
-let output = ""
-for (let i= 0; i < loop.length;i ++){
-
-if (loop[i] === value) {
-  output = key[i]
-
-}
-}
-return output
-}
-const bestTVShowsByGenre = {
-sci_fi: "The Expanse",
-comedy: "Brooklyn Nine-Nine",
-drama:  "The Wire"
-};
-
-console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
-console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"));
-console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
-console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
-console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
 
 module.exports = findKeyByValue;
