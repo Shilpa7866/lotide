22d5d7397f7fcca12273c7611015bfdb1738916e
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-26 14:10:46 -0400

    adding countOnlyTest.js file

---
+++ test/countOnlyTest.js
@@ -0,0 +1,46 @@
+// import assert library from chai package.
+const assert = require('chai').assert;
+
+// import countOnly function.
+const countOnly = require('../countOnly');
+
+describe("countOnly function", () => {
+  it("returns the correct count for specified items in the array", () => {
+    const items = [
+      "Karl",
+      "Salima",
+      "Agouhanna",
+      "Fang",
+      "Kavith",
+      "Jason",
+      "Salima",
+      "Fang",
+      "Joe"
+    ];
+    const result = countOnly(items, {
+      "Jason": true,
+      "Karima": true,
+      "Fang": true,
+      "Agouhanna": false });
+    assert.deepEqual(result["Jason"], 1);
+  });
+  it("returns undefined for an item that is not present in the object", () => {
+    const items = [
+      "Karl",
+      "Salima",
+      "Agouhanna",
+      "Fang",
+      "Kavith",
+      "Jason",
+      "Salima",
+      "Fang",
+      "Joe"
+    ];
+    const result =  countOnly(items, {
+      "Jason": true,
+      "Karima": true,
+      "Fang": true,
+      "Agouhanna": false });
+    assert.deepEqual(result["Karima"], undefined);
+  });
+});
