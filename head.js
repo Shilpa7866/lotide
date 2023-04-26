a5652b8d4634e835e36da975864615f4a3b614b2
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-26 14:47:41 -0400

    adding modified head.js file

--- head.js
+++ head.js
@@ -1,15 +1,6 @@
-
-const assertEqual = require('./assertEqual');
-
-const head = function(arr1) {
+const head = function (arr1) {
   return arr1[0];
 };
-
-
-console.log(assertEqual(head([5,6,7]), 5));
-
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-
 
 module.exports = head;
 
