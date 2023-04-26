e47e206ad5ebb4cfcd44170db24bffa8b9c425f1
Shilpa <shilpa.pulipati24@gmail.com>
2023-04-26 14:09:54 -0400

    adding eqObjectsTest.js file

---
+++ test/eqObjectsTest.js
@@ -0,0 +1,17 @@
+// import assert library from chai package.
+const assert = require('chai').assert;
+
+// import eqObjects function.
+const eqObjects = require('../eqObjects');
+
+describe("eqObjects function", () => {
+  it("returns true for objects with same properties", () => {
+    assert.deepEqual(eqObjects({color: 'red', size: 'medium'}, {size: 'medium', color: 'red'}), true);
+  });
+  it("returns true for objects with same array values", () => {
+    assert.deepEqual(eqObjects({colors: ['red', 'blue'], size: 'medium'}, { size: 'medium', colors: ['red', 'blue']}), true);
+  });
+  it("returns true for objects with same nested object values", () => {
+    assert.deepEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+  });
+});
